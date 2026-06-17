210d14ecc65e5b5ce396fae9ff76edb4bcb3e9da
Maria Clara Goncalves <mariaclaraestu@gmail.com>
2026-06-17 09:49:48 -0300

    Alteracoes em l031q07.js

--- l031q07.js
+++ l031q07.js
@@ -12,5 +12,6 @@ console.log ("Informe o terceiro valor inteiro") ;
 num3 = 7;
 
 media = (num1 + num2 + num3) / 3;
-
 console.log ("A média dos valores inseridos é "+ media);
+
+//Comentário realizado 17/06/26
